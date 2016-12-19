e1ddc31a0d5d7e252792c5373bd7c880f7a2b288
amansachdeva21 <aman.sachdeva21@gmail.com>
2016-12-19 16:09:37 +0000

    change the DB conncection

--- PreReqActvity/config/db.js
+++ PreReqActvity/config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
     module.exports = {
-        url : 'mongodb://localhost:27017/perf-db'
+        url : 'mongodb://aman.sachdeva21:sapient123@ds139448.mlab.com:39448/perf-db'
     }
