2568e155fa4d926b4327f54465e5991c491c1083
Aman Sachdeva <asachdeva5@sapient.com>
2016-12-21 01:41:58 +0000

    db changes

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
     module.exports = {
-        url : 'mongodb://heroku_mvccnplq:sapient123@ds139438.mlab.com:39438/heroku_mvccnplq'
+        url : 'mongodb://heroku_mvccnplq:5k40iegd6s438j31g42kmav2bs@ds139438.mlab.com:39438/heroku_mvccnplq?authMode=scram-sha1&rm.tcpNoDelay=true'
     }
