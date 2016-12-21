d2015e19b4fe84640bd685808a0e2396310cd6de
Aman Sachdeva <asachdeva5@sapient.com>
2016-12-21 01:08:33 +0000

    db config change

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ var prereqdata = require('./models/Nerd');
 
 var assert = require('assert');
 //var url = 'mongodb://localhost:27017/perf-db';
-var url = 'mongodb://heroku_mvccnplq:sapient123@ds139438.mlab.com:39438/heroku_mvccnplq';
+var url = 'mongodb://heroku_mvccnplq:5k40iegd6s438j31g42kmav2bs@ds139438.mlab.com:39438/heroku_mvccnplq';
 var MongoClient = require('mongodb').MongoClient;
 
 module.exports = function(app) {
